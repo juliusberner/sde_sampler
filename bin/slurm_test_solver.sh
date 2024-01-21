0c44c016888854c4eb96bcc7c6cbf3a18b894a72
Junhua Chen <junhua.chen.ios@gmail.com>
2024-01-21 14:02:14 -0800

    Rename basic_dds -> basic_dds_euler

--- bin/slurm_test_solver.sh
+++ bin/slurm_test_solver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Test all solvers
-python scripts/main.py -m +launcher=slurm solver=dis_subtraj,bridge_diff_init,bridge_subtraj,basic_bridge_subtraj,basic_pis,basic_dds,basic_bridge,dis_no_score,langevin,pis,basic_dis,bridge,pis_no_score,dis,basic_dis_subtraj,dds,dds_euler wandb.project=test ++train_steps=2
+python scripts/main.py -m +launcher=slurm solver=dis_subtraj,bridge_diff_init,bridge_subtraj,basic_bridge_subtraj,basic_pis,basic_dds_euler,basic_bridge,dis_no_score,langevin,pis,basic_dis,bridge,pis_no_score,dis,basic_dis_subtraj,dds,dds_euler wandb.project=test ++train_steps=2
