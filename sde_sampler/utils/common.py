53020bd0b256e7ded78d205210b2b9657f58aed3
Junhua Chen <junhua.chen.ios@gmail.com>
2024-01-21 06:59:07 -0800

    Add cosine discretization  function

--- sde_sampler/utils/common.py
+++ sde_sampler/utils/common.py
@@ -4,6 +4,7 @@ import math
 from collections import namedtuple
 
 import torch
+import numpy as np
 
 import wandb
 
@@ -34,6 +35,30 @@ def get_timesteps(
         return torch.sqrt(
             torch.linspace(start, end.square(), steps=steps + 1, device=device)
         ).clip(max=end)
+    elif rescale_t == "cosine":
+        """
+        Copied verbatim from 
+        https://github.com/franciscovargas/denoising_diffusion_samplers/blob/main/dds/discretisation_schemes.py#L50
+        """
+        s = 0.008 # Choice from original paper
+        pre_phase = torch.linspace(start, end, steps + 1, device=device) / end
+        phase = ((pre_phase + s) / (1 + s)) * np.pi * 0.5
+
+        dts = torch.cos(phase)**4
+
+        dts /= dts.sum()
+        dts *= end  # We normalise s.t. \sum_k \beta_k = T (where beta_k = b_m*cos^4)
+
+        dts_out = torch.concat((torch.tensor([start], device = device), torch.cumsum(dts,-1)))
+
+        # New Adaptation for this repo: 
+        # We remove excess 1s at end 
+        ptr = len(dts_out) 
+        eps = 1e-6
+        while abs(dts_out[ptr - 1] - end) < eps * end:
+            ptr -= 1
+
+        return dts_out[:ptr]
     raise ValueError("Unkown timestep rescaling method.")
 
 
